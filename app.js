3b92e7a8e3a4c96f793df4dfecdfedd625e4c902
Yunyun <celeph4is@gmail.com>
2020-04-28 16:45:26 -0700

    refactor app into modules; step2 complete

---
+++ app.js
@@ -0,0 +1,16 @@
+const config = require('./utils/config');
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const blogsRouter = require('./controllers/blogs');
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('connected to mongodb'))
+.catch(() => console.log('there was a problem connection to mongodb'))
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,43 +1,6 @@
-const config = require('./utils/config');
+const app = require('./app');
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('connected to mongodb'))
-.catch(() => console.log('there was a problem connection to mongodb'))
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const config = require('./utils/config');
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
