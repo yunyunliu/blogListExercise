dc54ece9c06ae77871bb1801e673ef09d4c72437
Yunyun <celeph4is@gmail.com>
2020-06-02 16:06:02 -0700

    create login router contain event handlers for login route, add loginRouter middleware to app. add token-based login functionality exercise 4.18 complete

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 .then(() => console.log('connected to mongodb'))
@@ -14,5 +15,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app;
--- controllers/login.js
+++ controllers/login.js
@@ -2,3 +2,29 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const body = request.body
+  const user = await User.findOne({ username: body.username })
+  const passwordCorrect = user === null // if user not in db, set passwordCorrect to false
+  ? false
+  : await bcrypt.compare(body.password, user.passwordHash) // if user in db, check password 
+
+  if(!(user && passwordCorrect)) { // i
+    return response.status(401).json({ // status code 401 means 'unauthorized'
+      error: 'invalid username or password'
+    })
+  }
+
+  const tokenUser = {
+    username: user.username,
+    id: user._id
+  }
+
+  const token = jwt.sign(tokenUser, process.env.SECRET) // create token, token is digitally signed using env variable SECRET
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
