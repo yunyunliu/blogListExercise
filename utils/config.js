471580267adb742cc6a94c899884911b88bd7a12
Yunyun <celeph4is@gmail.com>
2020-12-03 23:50:21 -0800

    change value of PORT

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 let MONGODB_URI = process.env.MONGODB_URI;
-let PORT = process.env.PORT;
+let PORT = process.env.PORT || 3001;
 
 module.exports = { MONGODB_URI, PORT };
